b0c1b2959d324f1d1077a1ecd6b601b0306af828
joaogt01 <joao.dev06@gmail.com>
2026-08-13 21:32:19 -0300

    feat: implementa PacienteRepository

---
+++ src/main/java/com/projetos/agendamento/paciente/repository/PacienteRepository.java
@@ -0,0 +1,10 @@
+package com.projetos.agendamento.paciente.repository;
+
+import com.projetos.agendamento.paciente.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Optional<Paciente> buscarUsuarioPorId(Long usuarioId);
+}
